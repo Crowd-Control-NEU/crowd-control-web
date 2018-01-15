cd42b8efda030cca8b203c86d8ad0b6bc564aac6
Joseph Lally <Lally.J@husky.neu.edu>
2018-01-15 18:16:57 -0500

    changed root page text

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send("Hello World!");
+    res.send("Crowd Control Homepage");
 });
 
 app.get('/About', (req, res) => {

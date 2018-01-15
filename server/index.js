3b0fe7336ab6792e7ad743cf24ea35e8ca0d1d6a
Joseph Lally <Lally.J@husky.neu.edu>
2018-01-15 18:11:05 -0500

    added about and room1 testing pages

--- server/index.js
+++ server/index.js
@@ -5,5 +5,13 @@ app.get('/', (req, res) => {
     res.send("Hello World!");
 });
 
+app.get('/About', (req, res) => {
+    res.send("This is the about page");
+});
+
+app.get('/Room1', (req, res) => {
+    res.send("This is the page to show details for ROOM 1!");
+});
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);

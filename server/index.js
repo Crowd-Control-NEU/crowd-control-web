1ea866ee6948892be0f245f9a9f515903af5b5e9
Joseph Lally <Lally.J@husky.neu.edu>
2018-01-15 18:42:14 -0500

    postgres

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const { Client } = require('pg');
 
 app.get('/', (req, res) => {
     res.send("Crowd Control Homepage");
@@ -15,3 +16,20 @@ app.get('/Room1', (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
+
+
+// connecting to DB
+const client = new Client({
+    connectionString: process.env.DATABASE_URL,
+    ssl: true,
+  });
+  
+  client.connect();
+  
+  client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
+    if (err) throw err;
+    for (let row of res.rows) {
+      console.log(JSON.stringify(row));
+    }
+    client.end();
+  });

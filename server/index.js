e7fcf31ce8ed978bc552ad357874dfd7612efe51
Joseph Lally <Lally.J@husky.neu.edu>
2018-01-16 18:09:44 -0500

    basic api call for getting people count at a location

--- server/index.js
+++ server/index.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const app = express();
 
+var db = require('./models/database');
+
+// a test api call
 app.get('/api/test', (req, res) => {
     res.send({ express: 'Hello From Node.js' });
+});
+
+// get the count at a certain location (for example, /count/Marino)
+app.get('/count/:location', (req, res) => {
+    db.getCountAtLocation(req.params.location, res);
 });
 
 const PORT = process.env.PORT || 5000;
--- server/models/database.js
+++ server/models/database.js
@@ -1,10 +1,10 @@
 var pg = require('pg');
 
 var conString = process.env.DB_URL || "postgres://postgres:5432@localhost/josephlally";
-var client = new pg.Client(conString);
 
 // setup initial table
 function createDefaultTable() {
+    var client = new pg.Client(conString);
     client.connect(function(err) {
         if(err) {
             return console.error('could not connect to postgres', err);
@@ -19,19 +19,22 @@ function createDefaultTable() {
 }
 
 // get current "people count" of a location
-function getCount(location) {
+var getCountAtLocation = function(location, res) {
+    var client = new pg.Client(conString);
     client.connect(function(err) {
         if(err) {
             return console.error('could not connect to postgres', err);
         }
-        client.query('SELECT * FROM data WHERE location_name=$1;', [location], function(err, result) {
+        client.query('SELECT SUM(count) FROM data WHERE location_name=$1;', [location], function(err, result) {
             if(err) {
                 return console.error('error running query', err);
             }
-            for (var i = 0; i < result.rows.length; i++) {
-                console.log(JSON.stringify(result.rows[i]))
+            else {
+                res.send(result.rows);
             }
             client.end();
         });
     });
 }
+
+module.exports.getCountAtLocation = getCountAtLocation;

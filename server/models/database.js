01cb6801a5f1b711629ce510dda64fb9341bba6e
Joseph Lally <Lally.J@husky.neu.edu>
2018-01-16 16:47:41 -0500

    create default table

--- server/models/database.js
+++ server/models/database.js
@@ -3,20 +3,19 @@ var pg = require('pg');
 var conString = process.env.DB_URL || "postgres://postgres:5432@localhost/josephlally";
 var client = new pg.Client(conString);
 
-function getRooms() {
+// setup initial table
+function createDefaultTable() {
     client.connect(function(err) {
         if(err) {
             return console.error('could not connect to postgres', err);
         }
-        client.query('SELECT * from rooms', function(err, result) {
+        client.query('CREATE TABLE IF NOT EXISTS data (id integer NOT NULL, location_name varchar(100), count integer, date TIMESTAMP); ', function(err, result) {
             if(err) {
                 return console.error('error running query', err);
             }
-            console.log("hey!");
             client.end();
-            return 100;
         });
     });
 }
 
-module.exports.getRooms = getRooms();
+createDefaultTable();

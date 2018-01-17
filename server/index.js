17c75837412c22080f502750bf2182389e1e0a76
Joseph Lally <Lally.J@husky.neu.edu>
2018-01-16 19:00:41 -0500

    post request /data-add started, no db entry yet

--- server/index.js
+++ server/index.js
@@ -1,7 +1,12 @@
 const express = require('express');
 const app = express();
 
-var db = require('./models/database');
+var bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+var db = require('./models/db');
 
 // a test api call
 app.get('/api/test', (req, res) => {
@@ -11,6 +16,16 @@ app.get('/api/test', (req, res) => {
 // get the count at a certain location (for example, /count/Marino)
 app.get('/count/:location', (req, res) => {
     db.getCountAtLocation(req.params.location, res);
+});
+
+// add a
+app.post('/data-add', (req, res) => {
+    var id = req.body.id;
+    var location_name = req.body.location_name;
+    var count = req.body.count;
+    var date = req.body.date;
+    console.log("Received: " + id, location_name, count, date);
+    res.send("yes");
 });
 
 const PORT = process.env.PORT || 5000;

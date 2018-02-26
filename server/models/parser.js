73648f735557e01c674a336ed2c6660d3741075c
justinvincelette <justinvincelette@gmail.com>
2018-02-26 14:03:29 -0500

    Added parsing functions for getting hourly averages

--- server/models/parser.js
+++ server/models/parser.js
@@ -1,6 +1,7 @@
 var moment = require('moment');
 
 // Get maximum total sum for each date
+// data: historical count data
 function getMaxSums(data) {
   var sums = {};
   var max = {};
@@ -17,6 +18,7 @@ function getMaxSums(data) {
 }
 
 // Get daily averages for given data
+// data: historical count data
 function getDailyAverages(data) {
   var occurrences = new Array(7).fill(0);
   var totals = new Array(7).fill(0);
@@ -33,4 +35,45 @@ function getDailyAverages(data) {
   return result;
 }
 
+// Get sum for each hour of days that fall on the given day of week
+// data: historical count data
+// day: int 0-6, representing day of the week
+function getHourlySums(data, day) {
+  var sums = {};
+  for (var i = 0; i < data.length; i++) {
+    var item = data[i];
+    var date = moment(item.date).format('L:H');
+    if (moment(date, "MM/DD/YYYY").day() == day) {
+      sums[date] = sums[date] || 0;
+      sums[date] += item.count;
+    }
+  }
+  return sums;
+}
+
+// Get hourly averages for day of week of given data
+// data: historical count data
+// day: int 0-6, representing day of the week
+function getHourlyAverages(data, day) {
+  var occurrences = new Array(24).fill(0);
+  var totals = new Array(24).fill(0);
+  var result = new Array(24).fill(0);
+  var sums = getHourlySums(data, day);
+  for (date in sums) {
+    var hourOfDay = moment(date, "MM/DD/YYYY:H").format('H');
+    totals[hourOfDay] += sums[date];
+    occurrences[hourOfDay]++;
+  }
+  for (var i = 0; i < 24; i++) {
+    // For hourly data, we want summed counts up to that point, not just the individual
+    // average count for a specific hour. So we need to add all the previous hour
+    // counts to the current hour
+    var countSoFar = i == 0 ? 0 : result[i - 1];
+    result[i] = totals[i] / occurrences[i];
+    result[i] += countSoFar;
+  }
+  return result;
+}
+
 module.exports.getDailyAverages = getDailyAverages;
+module.exports.getHourlyAverages = getHourlyAverages;

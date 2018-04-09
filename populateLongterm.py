562cc1dc3eb82fa2a242d42ce4c38b2de5ca95d1
Joseph Lally <Lally.J@husky.neu.edu>
2018-04-09 16:01:23 -0400

    update data population

--- populateLongterm.py
+++ populateLongterm.py
@@ -7,18 +7,18 @@ import random
 API_ENDPOINT_LOCAL = 'http://localhost:5000/data-add'
 API_ENDPOINT_PRODUCTION = 'https://infinite-peak-11670.herokuapp.com/data-add'
 
-API_TO_USE = API_ENDPOINT_LOCAL
-location = 'Subway'
+API_TO_USE = API_ENDPOINT_PRODUCTION
+location = 'Matthews Arena'
 
 days = 1000
 date_to_send = ''
-for x in range(60,days):
+for x in range(59,days):
     date = date.today() - timedelta(days=x)
     min_pub_date_time = datetime.combine(date, time.min) 
     time = datetime.time(min_pub_date_time + timedelta(minutes=15))
     date_to_send = datetime.combine(min_pub_date_time, time)
 
-    randomCount = random.randint(5 , 20)
+    randomCount = random.randint(150, 300)
 
     data = parse.urlencode({'count': randomCount, 'location_name': location, 'date': date_to_send}).encode()
     req =  request.Request(API_TO_USE, data=data) # this will make the method "POST"
